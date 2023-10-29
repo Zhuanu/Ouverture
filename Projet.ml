f1391507f3d420b5376fbe476dbf81bc0709037b
Zhuanu <zhuang.jeanmarc01@gmail.com>
2023-10-29 02:12:16 +0200

    composition à revoir à cause du bit le plus fort

--- Projet.ml
+++ Projet.ml
@@ -19,7 +19,7 @@ struct
   let length list = List.length !list  
 end ;;
 
-let rec bit65 n accu = if n <= 0 then accu@[true] else bit65 (n-1) (accu@[false]);;
+let rec bit64 n accu = if n <= 0 then accu else bit64 (n-1) (accu@[false]);;
 
 let decomposition l = 
   let rec loop x list =
@@ -33,7 +33,7 @@ let decomposition l =
     | [] -> list
     | hd::tl -> 
       if hd = 0L 
-        then loop2 tl (list @ (bit65 64 []))
+        then loop2 tl (list @ (bit64 64 []))
         else loop2 tl (loop hd list)
   in
   loop2 l [];;
@@ -46,7 +46,7 @@ let rec ajoutFalse list n accu =
 
        
 let rec suppElem list n accu =
-  if (n <= 0) then (List.rev accu)
+  if (n <= 0) then (List.rev accu, list)
   else match list with
     | [] -> failwith "Empty"
     | hd::tl -> suppElem tl (n-1) (hd::accu);;
@@ -54,36 +54,50 @@ let rec suppElem list n accu =
   
 let completion list n =
   let len = List.length list in
-  if (len <= n) then ajoutFalse list (n-len) []
-  else suppElem list n []
+  if (len <= n) then (ajoutFalse list (n-len) [], [])
+  else suppElem list n [];;
 
 
 (* let composition list = 
-  let rec loop l accu cpt =
+  let rec loop l accuElem cpt accuList =
     match l with
-    | [] -> accu
+    | [] -> accuElem::accuList
     | hd::tl -> 
-      if cpt = 0 
-        then loop tl (Int64.logor (Int64.shift_left accu 1) 1L) 0
-        else loop tl (Int64.logor (Int64.shift_left accu 1) 0L) (cpt-1)
-      if hd 
-        then loop tl (Int64.logor (Int64.shift_left accu 1) 1L) 0
-        else loop tl (Int64.logor (Int64.shift_left accu 1) 0L) (cpt+1)
-  in loop (List.rev list) 0L;; *)
+      if hd then 
+        if cpt = 64
+          then loop tl 0L 0 [0L]@accuList
+          else loop tl (Int64.logor (Int64.shift_left accuElem 1) 1L) 0 accuList
+      else loop tl (Int64.logor (Int64.shift_left accuElem 1) 0L) (cpt+1) accuList
+  in loop list 0L 0 [];; *)
 
-let composition list =
-  let rec loop l accu cpt = 
-    match l with
-    | [] -> accu
-    | hd::tl -> 
-      if hd 
-        then loop tl (Int64.logor (Int64.shift_left accu 1) 1L) 0
-        else loop tl (Int64.logor (Int64.shift_left accu 1) 0L) (cpt+1)
-    
-    
+  let package list = 
+    let rec loop l =
+      let (hd, tl) = completion l 64 in
+      match tl with
+      | [] -> [hd]
+      | _ -> hd::(loop tl)
+    in loop list;;
+
+  let composition list =
+    let rec loopExt l =
+      match l with
+      | [] -> []
+      | hd::tl -> 
+        let rec loopInt l accu =
+          match l with
+          | [] -> accu
+          | hd::tl -> 
+            loopInt tl (Int64.logor (Int64.shift_left accu 1) (if hd then 1L else 0L))
+        in loopInt (List.rev hd) 0L :: loopExt tl
+    in loopExt (package list);;
+
+    let print_list lst =
+      List.iter (fun x -> Printf.printf "%b\n" x) lst
+    ;;
 
 
 let table x n = 
   let binary_list = decomposition [(Int64.of_int x)] in
   completion binary_list n;;
+
        
